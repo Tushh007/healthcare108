ac5b25bb2408c37b4bce4f07663eb8b22c8dc5a0
Tushar <tushar.langer@gmail.com>
2019-09-07 14:02:05 +0000

    latest

--- healtcare108-frontend/src/app/app.component.ts
+++ healtcare108-frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'healtcare108-frontend';
+  title = 'Healthcare Analysis';
 }
