b20643f052970569d4058e048f9b66114599ed60
Tushar <tushar.langer@gmail.com>
2019-09-07 09:22:47 +0000

    second commit

--- healtcare108-frontend/src/app/core/core.module.ts
+++ healtcare108-frontend/src/app/core/core.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AppModule } from '../app.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    AppRoutingModule
+  ],
+  exports: [
+    NavigationComponent
   ]
 })
 export class CoreModule { }
